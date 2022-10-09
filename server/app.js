ccab4360ca4af7d556e2c6df67a0eb5ddbc7f00b
MelittaPacheco <melittapacheco@gmail.com>
2022-10-09 10:48:48 +0000

    Melitta/feature added: Single event

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser"
-import cors from 'cors'
-import router from "./routes/index.js";
-import connectDB from '../server/config/db.js'
+import connectDB from "./config/db.js";
+import router from "./routes/index.js"; 
 
 const app = express();
 
@@ -10,14 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
-
-app.use(router)
+app.use(router);
 
 app.get("/", (req, res) => {
   res.send("I'm Working!!");
