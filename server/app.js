6a91e55d19ce81a3daf1d61949100e440bbb0c25
praTHM23 <cspcce2023@gmail.com>
2022-10-11 16:11:09 +0530

    feat: Added team Event

--- server/app.js
+++ server/app.js
@@ -23,12 +23,7 @@ app.get("/", (req, res) => {
   res.send("I'm Working!!");
 });
 
-
-
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-//database connection
-
-connectDB();
