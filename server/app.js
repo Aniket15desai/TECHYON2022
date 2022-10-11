0073bb673661e35af8810809f7bb962ea19b7f2f
praTHM23 <cspcce2023@gmail.com>
2022-10-11 16:11:09 +0530

    feat: Added team Event

--- server/app.js
+++ server/app.js
@@ -1,21 +1,14 @@
 import express from "express";
+import mongoose from "mongoose"
 import bodyParser from "body-parser"
-import cors from 'cors'
 import router from "./routes/index.js";
 import connectDB from '../server/config/db.js'
 
 const app = express();
 
-// JSON
+ //JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
 
 app.use(router)
 
@@ -23,7 +16,12 @@ app.get("/", (req, res) => {
   res.send("I'm Working!!");
 });
 
+
+
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+//database connection
+
+connectDB();
